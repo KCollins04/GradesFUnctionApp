002feca3268efcccb5d27f6bcb8c84b111b751cb
ok <kelsey.collins23@d155.org>
2022-09-23 07:26:58 -0500

    Initial Commit

---
+++ GradesFUnctionApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GradesFUnctionApp
+//
+//  Created by KELSEY COLLINS on 9/23/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
