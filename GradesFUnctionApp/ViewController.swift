cace6b385cca28f8028548fb9a5fff4cf4847a51
ok <kelsey.collins23@d155.org>
2022-09-23 08:04:00 -0500

    yes

--- GradesFUnctionApp/ViewController.swift
+++ GradesFUnctionApp/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var outletTextGrade: UILabel!
+    @IBOutlet weak var outletInput: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func actionButtonGo(_ sender: Any) {
+        percentage(num1: Double(outletInput.text))
+            
+        }
+    func percentage(num1: Double){
+        if num1 <= 50 {
+         print("Try Agian")
+        }
+        else
+        { print ("You Passed")}
+        
+    }
+    
 }
 
